776992a78f7f75e382a8a0c54d35b8a368f23818
GabFiterman <gabriel@nimbusmeteorologia.com.br>
2024-05-29 16:11:13 -0300

    chore: ajust project to build with external webview

--- app-build/App.js
+++ app-build/App.js
@@ -3,10 +3,13 @@ import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
+    // const WEBVIEW_URI="http://192.168.1.5:4173";
+    const WEBVIEW_URI="https://webearts.com";
+
     return (
         <View style={styles.container}>
             <WebView
-                source={{ uri: 'http://192.168.1.5:4173' }}
+                source={{ uri: WEBVIEW_URI }}
                 style={{ flex: 1 }}
             />
         </View>
