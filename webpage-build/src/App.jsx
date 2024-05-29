47c0addd9ad98901afbc858690fb4ec4ab52f199
GabFiterman <gabriel@nimbusmeteorologia.com.br>
2024-05-29 16:19:50 -0300

    minor: update webpage text to new build

--- webpage-build/src/App.jsx
+++ webpage-build/src/App.jsx
@@ -7,7 +7,10 @@ function App() {
     return (
         <div className='webpageView'>
             <h1>Testing Webpage</h1>
-            <h2>Build 2</h2>
+            <h2>Build 3</h2>
+            <p>Neste teste, já espero ser um dos últimos.</p>
+            <p>Esta mesma build deve ser hospedada em diferentes lugares, e o app deve ser buildado apontando para a mesma.</p>
+            <p>Então adicionar um pouco da complexidade do app, como as tecnologias, e o principal: Testar os CORS, fazer requisição desta webpage pra API alertario. se precisar, teste fazer o servidor proxy.</p>
             <div className="rowContainer">
                 <button onClick={() => setCount((count) => count - 1)}>
                     -
