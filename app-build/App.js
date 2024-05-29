cb62f0ee1c0368f85147144774235b27ad5a9c2f
GabFiterman <gabriel@nimbusmeteorologia.com.br>
2024-05-29 20:15:16 -0300

    chore: update App to use envrionment variabes

--- app-build/App.js
+++ app-build/App.js
@@ -3,13 +3,11 @@ import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
-    // const WEBVIEW_URI="http://192.168.1.5:4173";
-    const WEBVIEW_URI="https://homolog.webearts.com";
-
+    const uri = process.env.EXPO_PUBLIC_WEBVIEW_URI;
     return (
         <View style={styles.container}>
             <WebView
-                source={{ uri: WEBVIEW_URI }}
+                source={{ uri: uri }}
                 style={{ flex: 1 }}
             />
         </View>
