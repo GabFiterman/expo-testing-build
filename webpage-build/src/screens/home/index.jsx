7648f08d7e4b7c547ba2f8b57c6f097cf039f35b
GabFiterman <gfiterman96@gmail.com>
2024-06-03 18:50:31 -0300

    minor: update webpage to build

--- webpage-build/src/screens/home/index.jsx
+++ webpage-build/src/screens/home/index.jsx
@@ -1,8 +1,9 @@
 const Home = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Home Page v0.3.0</h1>
             <p>Testando se nova build na webpage também atualiza o APK</p>
+            <p>Último teste desse. Próximo já vai ser com chamada pra API.</p>
         </div>
     );
 };
