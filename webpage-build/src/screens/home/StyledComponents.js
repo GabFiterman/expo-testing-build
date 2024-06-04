3f5b73fd4ea9c8b745cc3aa8f962590f4b77b27b
GabFiterman <gabriel@nimbusmeteorologia.com.br>
2024-06-04 02:42:17 -0300

    minor: update homePage to test

---
+++ webpage-build/src/screens/home/StyledComponents.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+    color: #2bc5b8;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    gap: 2rem;
+    padding: 1em;
+`;
--- webpage-build/src/screens/home/index.jsx
+++ webpage-build/src/screens/home/index.jsx
@@ -1,10 +1,11 @@
+import { HomeContainer } from "./StyledComponents";
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page v0.3.0</h1>
+        <HomeContainer>
+            <h1>Home Page v0.3.1</h1>
             <p>Testando se nova build na webpage também atualiza o APK</p>
             <p>Último teste desse. Próximo já vai ser com chamada pra API.</p>
-        </div>
+        </HomeContainer>
     );
 };
 
